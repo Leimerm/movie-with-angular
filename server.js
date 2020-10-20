8a6665d44c295985550310fad26d3007f2630fc2
Leimerm <matthewleimer@gmail.com>
2020-10-20 13:47:33 -0500

    new heroku

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require('express');
-var forceSsl = require('force-ssl-heroku');
 const path = require('path');
-const enableProdMode = require('@angular/core').enableProdMode
-
-enableProdMode();
-
 const app = express();
-
-app.use(forceSsl);
-
-app.use(express.static('./dist/movie-with-angular'));
-
-app.get('/*', (req,res)=>{
-  res.sendFile(path.join(__dirname, '/dist/movie-with-angular/index.html'))
-});
-
-app.listen(process.env.PORT || 8080, ()=>{
-  console.log('Server started')
-})
+app.use(express.static(__dirname + '/dist/movie-with-angular'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/movie-with-angular/index.html'));});
+app.listen(process.env.PORT || 8080);
