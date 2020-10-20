22d9af6e5e7ca2d5e54399e858df83787b7ed8d5
Leimerm <matthewleimer@gmail.com>
2020-10-20 17:55:34 -0500

    here we go

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const forceSsl = require('force-ssl-heroku');
 const path = require('path');
+const enableProdMode = require('@angular/core').enableProdMode
+enableProdMode();
 const app = express();
-app.use(express.static(__dirname + '/dist/movie-with-angular'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/movie-with-angular/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.use(forceSsl);
+app.use(express.static('./dist/movie-with-angular'));
+app.get('/*', (req,res)=>{
+  res.sendFile(path.join(__dirname, './dist/movie-with-angular/index.html'))
+});
+app.listen(process.env.PORT || 8080, ()=>{
+  console.log('Server started')
+});
